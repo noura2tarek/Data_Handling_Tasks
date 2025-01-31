9b5f002eb7eb9819759e114d85b8584ee4c4e713
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-01-31 21:38:11 +0200

    Add comments

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 import 'package:data_handling_task1/models/employee_model.dart';
 import 'package:data_handling_task1/services/shared_pref.dart';
@@ -8,21 +9,26 @@ class ApiService {
   String endPoint = 'https://mocki.io/v1/283ba093-9bf9-42e4-8f28-d2538937f9ca';
   Dio dio = Dio();
 
+  // Get users data method from the endpoint url
+  // Using dio package
   Future<List<EmployeeModel>> getUsersData() async {
     List<EmployeeModel> users = [];
-
+    // Handle any errors that may occur by putting my code in try-catch
     try {
       var response = await dio.get(endPoint);
       var data = response.data;
+      // Save the response data locally in shared preferences
+      // convert the response data to string
       String dataString = jsonEncode(data);
       CacheHelper.saveData(key: 'employeesList', value: dataString);
       if (response.statusCode == 200) {
+        // convert the response data to users list
         data.forEach((user) {
           users.add(EmployeeModel.fromJson(user));
         });
       }
     } catch (e) {
-      print('error occurred : $e');
+      log('error occurred : $e');
     }
     return users;
   }
--- lib/services/shared_pref.dart
+++ lib/services/shared_pref.dart
@@ -3,14 +3,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CacheHelper {
   static SharedPreferences? sharedPreferences;
 
+  // initialize shared preference
   static init() async {
     sharedPreferences = await SharedPreferences.getInstance();
   }
 
+  // Save string key in shared preferences
   static saveData({required String key, required String value}) async {
     await sharedPreferences?.setString(key, value);
   }
 
+  // Get string key from shared preferences
   static Future<String?> getData({required String key}) async {
     return sharedPreferences?.getString(key);
   }
